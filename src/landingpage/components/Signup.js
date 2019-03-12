9e1a08718e790df21eae3c6636faeccb6717189b
Jerrica Bobadilla <>
2019-03-11 17:36:07 -0700

    error handling for creating a user

--- src/landingpage/components/Signup.js
+++ src/landingpage/components/Signup.js
@@ -10,21 +10,36 @@ import axios from 'axios'
 // ==============================
 function Signup(props) {
   // ==============================
-  // FORM
+  // STATE
   // ==============================
-  // -- state
+  // -- form
   const [email, setEmail] = useState('')
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [display_name, setDisplay_name] = useState('')
+  // -- error handling
+  const [emailError, setEmailError] = useState({
+    status: false,
+    message: ''
+  })
+  const [usernameError, setUsernameError] = useState({
+    status: false,
+    message: ''
+  })
+  const [passwordError, setPasswordError] = useState({
+    status: false,
+    message: ''
+  })
 
-  // -- handlers
-  // handles form submit
+  // ==============================
+  // HANDLERS
+  // ==============================
+  // -- handles form submit
   function handleSubmit(e) {
     // prevent default
      e.preventDefault()
     // create the user
-    handleCreateUser({
+    handleCreateValidity({
       email: email,
       username: username,
       password: password,
@@ -32,29 +47,49 @@ function Signup(props) {
     })
   }
 
-  // handles checking whether or not the user input is valid
-  function handleValidity(e) {
+  // -- handles checking whether or not the user input is valid
+  function handleInputValidity(e) {
     switch(e.target.name) {
       case 'username':
-        if(e.key.match(/[^\w.]/)) {
+        if(checkUsername(e.key)) {
           e.preventDefault()
         }
-        break;
-      case  'password':
-        if(e.which === 32) {
+        break
+      case 'password':
+        if(checkPassword(e.key)) {
           e.preventDefault()
         }
-        break;
+        break
       default:
-        break;
+        break
     }
   }
 
-  // ==============================
-  // AXIOS REQUESTS
-  // ==============================
-  // create a new user
+  // -- handles checking whether what's been submitted is valid
+  function handleCreateValidity(newUser) {
+    // checks username, if invalid set error state
+    if(checkUsername(newUser.username)) {
+      setUsernameError({
+        status: true,
+        message: 'invalid username, only alphanumeric characters, underscores, and periods allowed'
+      })
+    }
+    // checks password, if invalid set error state
+    if(checkPassword(newUser.password)) {
+      setPasswordError({
+        status: true,
+        message: 'invalid password, no spaces allowed'
+      })
+    }
+    // checks if both are false, if so then create
+    if(!checkPassword(newUser.password) && !checkUsername(newUser.username)) {
+      handleCreateUser(newUser)
+    }
+  }
+
+  // -- handles creating a new user
   function handleCreateUser(newUser) {
+    // create the user
     axios.post('http://localhost:3000/users', newUser)
       .then((createdUser) => {
         console.log(createdUser)
@@ -64,7 +99,42 @@ function Signup(props) {
         setPassword('')
         setDisplay_name('')
       })
-      .catch(err => console.log(err))
+      .catch((err) => {
+        switch(err.response.data) {
+          case 'email':
+            setEmailError({
+              status: true,
+              message: 'email already registered! try logging in or use another email'
+            })
+            break
+          case 'username':
+            setUsernameError({
+              status: true,
+              message: 'sorry, username taken! please choose another'
+            })
+            break
+          default:
+            // PUT 404 HERE LATER
+            break
+        }
+      })
+  }
+
+  // ==============================
+  // ETC. HELPER METHODS
+  // ==============================
+  function checkUsername(username) {
+    if(username.match(/[^\w.]/)) {
+      return true
+    }
+    return false
+  }
+
+  function checkPassword(password) {
+    if(password.match(" ")) {
+      return true
+    }
+    return false
   }
 
   // ==============================
@@ -88,7 +158,11 @@ function Signup(props) {
             placeholder="EMAIL ADDRESS"
             value={email}
             onChange={e => setEmail(e.target.value)}
+            required
           />
+          { emailError.status ?
+              <div className="user-create-error">{emailError.message}</div> : null
+          }
         </label>
         <label required className="new-username">
           <span>username <strong>*</strong></span>
@@ -98,8 +172,13 @@ function Signup(props) {
             placeholder="USERNAME"
             value={username}
             onChange={e => setUsername(e.target.value)}
-            onKeyPress={handleValidity}
+            onKeyPress={handleInputValidity}
+            maxLength="15"
+            required
           />
+          { usernameError.status ?
+              <div className="user-create-error">{usernameError.message}</div> : null
+          }
         </label>
         <label required className="new-password">
           <span>password <strong>*</strong></span>
@@ -109,8 +188,12 @@ function Signup(props) {
             placeholder="PASSWORD"
             value={password}
             onChange={e => setPassword(e.target.value)}
-            onKeyPress={handleValidity}
+            onKeyPress={handleInputValidity}
+            required
           />
+          { passwordError.status ?
+              <div className="user-create-error">{passwordError.message}</div> : null
+          }
         </label>
         <label className="new-displayname">
           <span>display name</span>
@@ -120,6 +203,7 @@ function Signup(props) {
             placeholder="DISPLAY NAME"
             value={display_name}
             onChange={e => setDisplay_name(e.target.value)}
+            maxLength="40"
           />
         </label>
         <input type="submit" value="create and get thinking!"/>
