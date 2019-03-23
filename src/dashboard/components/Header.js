78f121930a3302b0c84dcb81aaabb1a05f6a6a93
Jerrica Bobadilla <>
2019-03-22 21:01:23 -0700

    profile nav styling

--- src/dashboard/components/Header.js
+++ src/dashboard/components/Header.js
@@ -12,14 +12,17 @@ function Header({ handleLogout }) {
 
   function handleProfileNavigation () {
     // get nav
+    let profileNavContainer = document.getElementsByClassName('dashboard-nav-container')[0].style
     let profileNavElement = document.getElementsByClassName('dashboard-nav')[0].style
 
     // toggles the profile navigation
     if(profileNav) {
-      profileNavElement.width = "0px"
+      profileNavElement.width = "0%"
+      profileNavContainer.opacity = "0"
       useProfileNav(false)
     } else {
       profileNavElement.width = "60%"
+      profileNavContainer.opacity = "1"
       useProfileNav(true)
     }
   }
@@ -38,13 +41,26 @@ function Header({ handleLogout }) {
           <img src="http://placehold.it/50x50.png" alt="user icon"/>
         </div>
       </div>
-      <div className="dashboard-nav">
-        <h1>@ username</h1>
-        <ul>
-          <li><button onClick={handleLogout}>logout</button></li>
-          <li><a href="/">link</a></li>
-          <li><a href="/">link</a></li>
-        </ul>
+      <div className="dashboard-nav-container">
+        <div className="dashboard-nav">
+          <div className="dashboard-nav-user">
+            {/* user names */}
+            <h1>display name</h1>
+            <h2>@username</h2>
+            {/* stats */}
+            <ul>
+              <li><strong>0</strong> Worlds</li>
+              <li><strong>0</strong> Stories</li>
+            </ul>
+          </div>
+          <div className="dashboard-nav-links">
+            <ul>
+              <li><a href="/"><span className="fas fa-user-circle"></span>View Profile</a></li>
+              <li><a href="/"><span className="fas fa-cog"></span>Profile Settings</a></li>
+              <li><a onClick={handleLogout}><span className="fas fa-sign-out-alt"></span>Sign Out</a></li>
+            </ul>
+          </div>
+        </div>
       </div>
     </span>
   )
